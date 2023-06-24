fa0ba800606284a2cb34af8e310b82da4cf8735f
atlantis333 <azcert20infra.cip.vip@gmail.com>
2023-06-24 10:34:12 +0700

    ytes

---
+++ AspNetCoreTests.Web/Models/CustomerModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AspNetCoreTests.Web.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        public string Address { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ AspNetCoreTests.Web/Services/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetCoreTests.Web.Models;
+
+namespace AspNetCoreTests.Web.Services
+{
+    public interface ICustomerService
+    {
+        Task<IList<CustomerModel>> List();
+        Task<CustomerModel> GetCustomer(int id);
+        Task SaveCustomer(CustomerModel customer);
+    }
+}
