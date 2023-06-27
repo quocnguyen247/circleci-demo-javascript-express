a0679cf6338eec2cf675634da76e459cf9970136
atlantis333 <azcert20infra.cip.vip@gmail.com>
2023-06-27 22:28:00 +0700

    testing

--- docker-run-unittests.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-set -eu -o pipefail
-
-dotnet restore /code/
-dotnet test /code/DotNetCoreSampleApi.Tests
--- src/TodoService/Program.cs
+++ src/TodoService/Program.cs
@@ -8,18 +8,21 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace DotNetCoreSampleApi
+namespace TodoService
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
+
+            host.Run();
+        }
     }
 }
